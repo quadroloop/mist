9e3df20634eae9e6bc796cad997a941ceea5c866
Bryce Mercines <nplixel@gmail.com>
2020-04-30 00:25:37 +0800

    saving progress on ui

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <i className="la la-atom la-3x logo" />
+
+      <div className="nav-group">
+        <input className="search" placeholder="Search" />
+      </div>
+    </div>
+  )
+}
+
+export default Header;
