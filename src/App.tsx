5bfebb2b163ba63da3b6049c47976fdd3f77821b
Bryce Mercines <nplixel@gmail.com>
2020-05-10 07:17:54 +0800

    created stat cards

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,28 @@ function App() {
       </div>
 
       <div className="content-body">
-
+        <div className="row">
+          <div className="col-md-4">
+            <div className="mist-card">
+              <p>Files</p>
+              <h3>32</h3>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="mist-card">
+              <p>Node Size</p>
+              <h3>4 <small>MB</small></h3>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="mist-card">
+              <p>Last update</p>
+              <h3>April 10, 2020</h3>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <p>this is not cool</p>
       </div>
     </div >
   );
