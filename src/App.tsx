0639cc564f74b61baf25e824e91004a85518226c
Bryce Mercines <nplixel@gmail.com>
2020-05-10 07:49:10 +0800

    created list of files

--- src/App.tsx
+++ src/App.tsx
@@ -26,12 +26,72 @@ function App() {
           <div className="col-md-4">
             <div className="mist-card">
               <p>Last update</p>
-              <h3>April 10, 2020</h3>
+              <h3>Dec 20, 2020</h3>
             </div>
           </div>
         </div>
         <hr />
-        <p>this is not cool</p>
+        <p><i className="la la-list mr-1" /> List of assets</p>
+        <hr />
+        <div className="file-list-wrapper">
+
+          <div className="file-item"
+          >
+            <img src="https://picsum.photos/200/300" alt="asset-avatar" />
+            <span>file.js</span>
+            <span>3 MB</span>
+            <span>april 20, 2020</span>
+          </div>
+
+          <div className="file-item"
+          >
+            <img src="https://picsum.photos/200/300" alt="asset-avatar" />
+            <span>file.js</span>
+            <span>3 MB</span>
+            <span>april 20, 2020</span>
+          </div>
+
+          <div className="file-item"
+          >
+            <img src="https://picsum.photos/200/300" alt="asset-avatar" />
+            <span>file.js</span>
+            <span>3 MB</span>
+            <span>april 20, 2020</span>
+          </div>
+
+          <div className="file-item"
+          >
+            <img src="https://picsum.photos/200/300" alt="asset-avatar" />
+            <span>file.js</span>
+            <span>3 MB</span>
+            <span>april 20, 2020</span>
+          </div>
+
+          <div className="file-item"
+          >
+            <img src="https://picsum.photos/200/300" alt="asset-avatar" />
+            <span>file.js</span>
+            <span>3 MB</span>
+            <span>april 20, 2020</span>
+          </div>
+
+          <div className="file-item"
+          >
+            <img src="https://picsum.photos/200/300" alt="asset-avatar" />
+            <span>file.js</span>
+            <span>3 MB</span>
+            <span>april 20, 2020</span>
+          </div>
+          <div className="file-item"
+          >
+            <img src="https://picsum.photos/200/300" alt="asset-avatar" />
+            <span>file.js</span>
+            <span>3 MB</span>
+            <span>april 20, 2020</span>
+          </div>
+
+
+        </div>
       </div>
     </div >
   );
